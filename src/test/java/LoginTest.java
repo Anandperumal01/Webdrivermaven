f0bbdd62a985eae1f329a21675a758240c75e7be
Tester1 <anandperumalece@gmail.com>
2019-06-05 11:17:23 +0530

    inital comment

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	//public static WebDriver driver;
+	
+	@BeforeTest
+	public void setup() {
+		
+		
+		//driver =new ChromeDriver();
+		System.out.println(" inital step up done");
+		
+	}
+	
+	@Test
+	public void launchguru99page() {
+		System.out.println(" open the guru 99 page");
+		
+	}
+	
+	@AfterTest
+	public void teardown() {
+		
+		
+		
+		System.out.println(" closed the driver");
+	}
+
+}
